ea477e98dc48b77133c3a8ae2ea007ac905fbf6c
Dru-Daniels <dancergirl_23@hotmail.com>
2021-02-23 03:37:22 -0500

    forgot this file

--- client/src/components/ArtistIndex.js
+++ client/src/components/ArtistIndex.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
+
+import CatMouse from '../assets/scss/images/catWitMouse.png'
 
 import ArtistList from "./ArtistList"
 
@@ -22,6 +25,25 @@ const ArtistIndex = props => {
     }
   }
 
+
+
+  let message; 
+  if (artists.length > 0) {
+    message = (
+      <div className='practice-title-container'>
+        <h1 className='text-center' id="practice-title">Practice Pad</h1>
+      </div>
+    )
+  } else {
+    message = (
+      <div className='practice-title-container'>
+        <h1 className='text-center' id="song-form-title-main">Uh oh! You need to add some songs!</h1>
+        <Link to='/songs/new'><h4 className='center-text practice-text' id='song-form-title'>Click Me!</h4></Link>
+        <img className='cat-mouse' src={CatMouse}/>
+      </div>
+    )
+  }
+
   useEffect(() => {
     window.scrollTo(0, 0)
     getArtists()
@@ -39,6 +61,7 @@ const ArtistIndex = props => {
 
   return (
     <div className="background-runner">
+        {message}
         <div className=''>
           {artistListTiles}
         </div>
